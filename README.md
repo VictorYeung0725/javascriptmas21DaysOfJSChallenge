dff68be9625e8040d1717c1684a7c885ef86b586
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2022-12-08 17:36:14 +0800

    finished the day 5 of the challenge at 8/12/2022

---
+++ README.md
@@ -0,0 +1,4 @@
+A Challenge from Scrimba JavaScriptmas 2022! 🎄
+a new challenge of Javascript will be released everyday
+From December 1st to 24th
+https://scrimba.com/learn/javascriptmas/-welcome-to-javascriptmas-2022-cv2BLQT8
---
+++ day5/day5.js
@@ -0,0 +1,41 @@
+import products from './data.js';
+
+/*
+   It's the day after Halloween 🎃 and all the candy is on sale!
+   
+   To buy up all the candy, use map() and filter() to put all the
+   candy into a `shoppingCart` array. 
+   
+   The new array should contain only the item and the price, like
+   this: 
+   
+   Expected output: 
+   [
+       {item: "🍭", price: 2.99},
+       {item: "🍫", price: 1.99}, 
+       {item: "🍬", price: 0.89}
+    ]
+*/
+
+let shoppingCart = [];
+
+//NOTE 1
+function getSaleItems(data) {
+  data.map((each) => {
+    if (each.type === 'sweet') {
+      const { item, price } = each;
+      console.log(item, price);
+      let shoppingItem = {
+        item: item,
+        price: price,
+      };
+      shoppingCart.push(shoppingItem);
+    }
+  });
+  return shoppingCart;
+}
+
+getSaleItems(products);
+console.log(shoppingCart);
+
+// checkSweet(products);
